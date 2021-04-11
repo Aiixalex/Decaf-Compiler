0d4728a8591ddc568b7177c10f9bdb9a8521557e
Aiixalex <wyl2000@zju.edu.cn>
2021-04-11 19:44:47 +0800

    delete src and contrib

--- contrib/build.sh
+++
@@ -1,6 +0,0 @@
-cd answer
-rm decafcomp
-make decafcomp
-cd ..
-python3 zipout.py
-python3 check.py
--- contrib/test.sh
+++
@@ -1,5 +0,0 @@
-cd answer
-rm decafcomp
-make decafcomp
-cd ..
-./answer/llvm-run testcases/dev/err-bool-index.decaf
